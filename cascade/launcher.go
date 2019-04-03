4704a8829ce933403e5f5c61334d7dc0cfd70c40
Craumix <soeren@guertler.net>
2019-04-03 23:48:52 +0200

    finished launcher

--- cascade/launcher.go
+++ cascade/launcher.go
@@ -1,12 +1,54 @@
 package main
 
+import (
+	"io/ioutil"
+	"os/exec"
+	"sync"
+)
+
 type exepack struct {
-	data       []byte
-	aeskey     []byte
-	encrypted  bool
-	compressed bool
+	data         []byte
+	aeskey       []byte
+	filename     string
+	isencrypted  bool
+	iscompressed bool
 }
 
-func main() {
+var executables []exepack
+var wg sync.WaitGroup
 
+func main() {
+	runCascade()
+}
+
+func runCascade() {
+	for _, exe := range executables {
+		go runExepack(exe, "")
+		wg.Add(1)
+	}
+	wg.Wait()
+}
+
+func runExepack(pack exepack, path string) {
+	//Encrypting compressed data can be unsafe
+
+	defer wg.Done()
+
+	b := pack.data
+	var err error
+	exepath := path + "/" + pack.filename
+
+	if pack.isencrypted {
+		b, err = Decrypt(pack.aeskey, b)
+		logif(err)
+	}
+	if pack.iscompressed {
+		b, err = gUnzipData(b)
+		logif(err)
+	}
+
+	err = ioutil.WriteFile(exepath, b, 0777)
+	logif(err)
+
+	exec.Command("cmd", "/c", exepath)
 }
