6797326544b14756efd09146d427a2c2cc7f66b2
Craumix <soeren@guertler.net>
2019-04-04 00:24:38 +0200

    launcher build

--- cascade/launcher_builder.go
+++ cascade/launcher_builder.go
@@ -1,5 +1,35 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
+func main() {
+	encrypt := readBool("Encrypt?")
+	compress := readBool("Compress?")
+	_ = encrypt
+	_ = compress
+}
+
+func readBool(msg string) bool {
+	s := strings.ToLower(readString(msg + " [y/n]"))
+	if s == "y" {
+		return true
+	} else if s == "n" {
+		return false
+	} else {
+		fmt.Println("Invalid!")
+		return readBool(msg)
+	}
+}
+
+func readString(msg string) string {
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Print(msg + " ")
+	input, _ := reader.ReadString('\n')
+	return strings.ReplaceAll(input, "\n", "")
 }
